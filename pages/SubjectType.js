37e0dd7c5ea4a479e15e75c04652c37105d22590
Diki <dikibhuyan@gmail.com>
2019-10-08 23:10:06 +0530

    basic pages build

---
+++ pages/SubjectType.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import SubjectTypeForm from '../forms/SubjectType'
+require('dotenv').config()
+
+export default class SubjectType extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username: "dikibhuyan",
+            role:'student',
+            logged: true
+        }
+    }
+    async componentDidMount() {
+        await fetch('http://'+SERVER_URL+'/subjecttype/', {
+            method: 'GET',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded",
+                'Accept': 'application/json',
+            }
+        })
+            .then(response => response.json())
+            .then(data => this.setState({
+                fields_data: data
+            }))
+    }
+    render() {
+        return (
+            <div>
+                <div className="container-fluid">
+                        <div className="row">
+                            <div className="col">
+                            <NavBar username={this.props.info.username}/>
+                            </div>
+                        </div>
+                        <div className="row my-3">
+                            <SideBar role={this.props.info.role}/>
+                            <div className="row card card-body mx-4">
+                                <div id='Body'>
+                                    <button type="button" className="btn btn-primary my-2" data-toggle="modal" data-target="#postform">
+                                        Create
+                                    </button>
+                                    <Post postform={SubjectTypeForm()}/>
+                                    <MDBDataTable striped bordered hover data={data} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <link rel="stylesheet" href="./static/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="./static/css/SideBar.css"/>
+                <script src="./static/js/jquery-3.3.1.slim.min.js"></script>
+                <script src="./static/js/popper.min.js"></script>
+                <script src="./static/js/bootstrap.min.js"></script>
+            </div>
+            )
+    }
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import App from '../components/App'
+import BatchFrom from '../forms/Batch'
 
 
 export default class dashboard extends React.Component {
@@ -7,17 +7,37 @@ export default class dashboard extends React.Component {
         super()
         this.state = {
             username: "dikibhuyan",
-            password: "qwertyuiop",
             role:'student',
-            logged: false,
-            Token: '9988776655544332211',
-            section:''
+            logged: false
         }
     }
     render() {
         return (
             <div>
-                <App info={this.state}/>
+                <div className="container-fluid">
+                        <div className="row">
+                            <div className="col">
+                            <NavBar username={this.props.info.username}/>
+                            </div>
+                        </div>
+                        <div className="row my-3">
+                            <SideBar role={this.props.info.role}/>
+                            <div className="row card card-body mx-4">
+                                <div id='Body'>
+                                    <button type="button" className="btn btn-primary my-2" data-toggle="modal" data-target="#postform">
+                                        Create
+                                    </button>
+                                    <Post postform={BatchFrom()}/>
+                                    <MDBDataTable striped bordered hover data={data} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <link rel="stylesheet" href="./static/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="./static/css/SideBar.css"/>
+                <script src="./static/js/jquery-3.3.1.slim.min.js"></script>
+                <script src="./static/js/popper.min.js"></script>
+                <script src="./static/js/bootstrap.min.js"></script>
             </div>
             )
     }
